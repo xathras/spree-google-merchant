cd43a36d16b449dcad0ffb3ad166ed6b31067c3a
jumph4x <visible@jumph4x.net>
2012-03-27 16:55:51 -0400

    Spree 1-0 migration

--- lib/spree_google_base/engine.rb
+++ lib/spree_google_base/engine.rb
@@ -15,5 +15,20 @@ module SpreeGoogleBase
     end
 
     config.to_prepare &method(:activate).to_proc
+    
+    initializer "spree_google_base.attr_map" do |app|
+      # See http://base.google.com/support/bin/answer.py?hl=en&answer=78170 for all other fields
+      GOOGLE_BASE_ATTR_MAP = [
+        ['g:id', 'id'],
+        ['g:mpn', 'sku'],
+        ['title', 'name'],
+        ['description', 'google_base_description'],
+        ['g:price', 'price'],
+        ['g:condition', 'google_base_condition'],
+        ['g:product_type', 'google_base_product_type'],
+        ['g:brand', 'google_base_brand'],
+        ['g:quantity','on_hand']
+      ]
+    end
   end
 end
